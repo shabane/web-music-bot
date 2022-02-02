0f37fe02278db6f1f4993bc97fcc996b371ae3e0
shabane <m.mohamadshabane@gmail.com>
2022-02-02 19:59:47 +0330

    init commit

---
+++ href.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+import argparse
+import json
+import sys
+
+
+with open("headers", "r") as fli:
+    fli = json.load(fli)
+    if type(fli) != dict:
+        print("err: the header file should be a json file", file=sys.stderr)
+    elif type(fli) == dict:
+        headers = fli
+    else:
+        headers = {}
+
+
+musics = set()
+# finding the all tag a and link's
+def GetAllTags(url: str, pattern: str):
+
+    site = requests.get(url, headers=headers)
+
+    if site.status_code != 200:
+        return str("err:", site.status_code, file=sys.stderr)
+    else:
+        site = bs(site.content, "html.parser")
+        site = site.find_all("a")
+        for i in site:
+            try:
+                if re.search(f"{pattern}", i["href"], re.IGNORECASE):
+                    musics.add(re.search(f"{pattern}", i["href"], re.IGNORECASE).string)
+            except:
+                return str("err:", site.status_code, file=sys.stderr)
+        return musics
+
+
+
